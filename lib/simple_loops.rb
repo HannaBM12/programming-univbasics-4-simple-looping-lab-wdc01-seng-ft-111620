3da752cb125feb70e5edce67952d9652ac2f8e4c
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-15 23:42:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,13 @@ def loop_message_n_times(string, n)
     count +=1
   end
 end
+
+
+def output_array(array)
+  count=0
+  while array[count] do
+    puts array[count]
+    count +=1
+  end
+end
+  
